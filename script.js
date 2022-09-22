b6be1bc8fb4a91a2234468b5fa0b4c0d1db285c0
Kyle Webb <Kylewebb3577@gmail.com>
2022-09-22 20:44:24 +0100

    refactoring

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
 const text = document.querySelector(".country");
+const countries = ["Spain","France","Italy","Turkey","Greece","Cyprus","Portugal","Croatia"];
+let i = 0;
 
 const animation = () => {
-    setTimeout(() => {
-        text.textContent = "Spain";
-    }, 0);
-    setTimeout(() => {
-        text.textContent = "France";
-    }, 4000);
-    setTimeout(() => {
-        text.textContent = "Italy";
-    }, 8000);
-    setTimeout(() => {
-        text.textContent = "Turkey";
-    }, 12000);
-    setTimeout(() => {
-        text.textContent = "Greece";
-    }, 16000);
-    setTimeout(() => {
-        text.textContent = "Cyprus";
-    }, 20000);
-    setTimeout(() => {
-        text.textContent = "Portugal";
-    }, 24000);
-    setTimeout(() => {
-        text.textContent = "Croatia";
-    }, 28000);
+    
+        text.textContent = countries[i++];
+        if(i == countries.length)
+        {
+            i = 0;
+        }
 }
 
-animation();
-setInterval(animation, 32000);
+//animation();
+setInterval(animation, 4000);
